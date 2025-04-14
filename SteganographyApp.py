fa4180e132ea50fa089180d78ad91eb7111d25b6
tweixelman <tweixelman@unomaha.edu>
2025-04-14 02:23:44 +0000

    Done with lab

--- SteganographyApp.py
+++ SteganographyApp.py
@@ -97,29 +97,46 @@ def numberToBinary(num):
   """Takes a base10 number and converts to a binary string with 8 bits"""
   binary = ""
   #Convert from decimal to binary
+  while num > 0:
+    binary = str(num % 2) + binary
+    num = num // 2
 
+  while len(binary) < 8:
+    binary = "0" + binary
 
   return binary
 
 def binaryToNumber(bin):
   """Takes a string binary value and converts it to a base10 integer."""
   decimal = 0
+  value = 1
+
+  while len(bin) > 0:
+    lastSpot = len(bin) - 1
+    lastDigit = bin[lastSpot]
+
+    if lastDigit == '1':
+      decimal = decimal + value
+    value = value * 2
+    bin = bin[0:lastSpot]
 
 
   return decimal
 
 def main():
   #Ask user if they want to encode/decode
-  myImg = Image.open('pki.png')
+  """myImg = Image.open('pki.png')
   myMsg = "This is a secret message I will hide in an image."
   encode(myImg, myMsg)
-  myImg.close()
+  myImg.close()"""
 
-  """
+ 
   yourImg = Image.open('secretImg.png')
   msg = decode(yourImg)
   print(msg)
-  """
-    
+ 
+   
 if __name__ == '__main__':
   main()
+
+
